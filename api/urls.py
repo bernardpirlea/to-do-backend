0a42fd123603ba95e502833faf2f8bb807d5cc0f
Bernard Pirlea <pirlea.bernard@gmail.com>
2021-10-05 22:12:16 +0300

    Implemeted register and login better and changed permisions

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken import views as auth_views
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
     path('register/', views.register, name='api-register'),
-    path('token-auth/', auth_views.obtain_auth_token, name='api-login'),
+    path('login/', auth_views.obtain_auth_token, name='api-login'),
     path('todo-list/', views.todoList, name='todo-list'),
     path('todo-create/', views.todoCreate, name="todo-Create"),
     path('todo-delete/<str:pk>', views.todoDelete, name="todo-Delete"),
--- api/views.py
+++ api/views.py
@@ -2,26 +2,26 @@ from django.utils import tree
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 from .serializers import ToDoSerializer, UserSerializer
 from .models import ToDo
-
-"""
-API Overview
-"""
 
 
 @api_view(['POST'])
 def register(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        print(serializer.errors)
-    return Response(serializer.data)
+    if 'email' in request.data and request.data['email']:
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.get_or_create(user=user)
+        else:
+            return Response({'error': 'Username already exists!'})
+        return Response({"token": str(token[0])})
+    return Response({'error': 'Email field is required!'})
 
 
-@api_view(['GET'])
-@permission_classes((IsAuthenticated,))
+@ api_view(['GET'])
+@ permission_classes((IsAuthenticated,))
 def apiOverview(request):
     api_urls = {
         'List': '/task-list/',
@@ -33,21 +33,21 @@ def apiOverview(request):
     return Response(api_urls)
 
 
-@api_view(['GET'])
-@permission_classes((IsAuthenticated,))
+@ api_view(['GET'])
+@ permission_classes((IsAuthenticated,))
 def todoList(request):
-    todos = ToDo.objects.all()
+    user = request.user
+    todos = ToDo.objects.filter(owner=user)
     serializer = ToDoSerializer(todos, many=True)
     return Response(serializer.data)
 
 
-@api_view(['POST'])
-@permission_classes((IsAuthenticated,))
+@ api_view(['POST'])
+@ permission_classes((IsAuthenticated,))
 def todoCreate(request):
 
     user = request.user
     todo = ToDo(owner=user)
-
     serializer = ToDoSerializer(todo, data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -56,8 +56,8 @@ def todoCreate(request):
     return Response(serializer.data)
 
 
-@api_view(['DELETE'])
-@permission_classes((IsAuthenticated,))
+@ api_view(['DELETE'])
+@ permission_classes((IsAuthenticated,))
 def todoDelete(request, pk):
     user = request.user
     todo = ToDo.objects.get(id=pk)
@@ -67,8 +67,8 @@ def todoDelete(request, pk):
     return Response("To do has been deleted successfully.")
 
 
-@api_view(['POST'])
-@permission_classes((IsAuthenticated,))
+@ api_view(['POST'])
+@ permission_classes((IsAuthenticated,))
 def todoUpdate(request, pk):
     user = request.user
     todo = ToDo.objects.get(id=pk)
