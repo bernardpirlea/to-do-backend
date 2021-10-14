454b30ce2a9fa94caa8b83e5e71464ce59686524
Bernard Pirlea <pirlea.bernard@gmail.com>
2021-10-14 23:32:01 +0300

    Configured cors module to allow req from react local app

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -42,12 +42,15 @@ INSTALLED_APPS = [
     # Third-Party Apps
     'rest_framework',
     'rest_framework.authtoken',
+    "corsheaders",
 
     # Local Apps
     'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
